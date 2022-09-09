ae9a46b83a2d1f24ae9b445a465f4fe3a816e1da
Olexandr <sashapena1337@gmail.com>
2022-09-10 01:46:54 +0300

    Version 1.0//Greetings, removing messages from bots, help

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 import telebot
 import sys
+from datetime import datetime, timedelta
 
 bot = telebot.TeleBot('5714768365:AAEetAVNSL3mUkkyBgWrKCq1y-xkUVF5WG0')
 
@@ -8,12 +9,29 @@ def start(message):
     mess = f'Привіт, {message.from_user.first_name} {message.from_user.last_name}'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
+@bot.message_handler(content_types=["sticker"])
+def start(message):
+    if message.sticker.file_unique_id == 'AgADwhgAArecOEg':
+        bot.delete_message(message.chat.id, message.id)
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    mess = f'/hello - привітання' \
+           f'\n/help - список команд'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+@bot.message_handler(commands=['help_me', 'save_me'])
+def start(message):
+    mess = f'Кочермякай, @{message.from_user.username}'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
 @bot.message_handler()
 def get_user_text(message):
     try:
         if message.via_bot:
             if message.via_bot.is_bot:
                 bot.delete_message(message.chat.id, message.id)
+    #        if message.from_user.username == "batislavskiy":
+#            bot.send_message(message.chat.id, f"@{message.from_user.username} ти <b>Агент Кремля</b>", parse_mode='html')
     except:
         print("Oops! occurred.")
 
